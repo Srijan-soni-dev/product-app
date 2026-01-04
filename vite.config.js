3da306065c3f6b7e112225ce889cd66f22814b5b
Srijan Soni <sriijan.soni27@gmail.com>
2026-01-05 01:16:56 +0530

    Updated project with new changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { BrowserRouter } from 'react-router-dom'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
+
+
+
