c1ba48a404cfee6b5b2106d8ab071c564b1db408
Srijan Soni <sriijan.soni27@gmail.com>
2026-01-05 19:31:45 +0530

    Fix BrowserRouter import and update project

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { BrowserRouter } from 'react-router-dom'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/product-app/", 
 })
 
 
